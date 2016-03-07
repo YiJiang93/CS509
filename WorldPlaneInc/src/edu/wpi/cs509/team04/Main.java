240772b25d8aab87a06eb20a476300094c5480b0
Daniel <amoghimi@wpi.edu>
2016-03-07 13:27:22 -0500

    Entry Point

---
+++ WorldPlaneInc/src/edu/wpi/cs509/team04/Main.java
@@ -0,0 +1,16 @@
+package edu.wpi.cs509.team04;
+
+/**
+ * Project EntryPoint
+ * 
+ * @author Daniel
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello WPI");
+	}
+
+}
