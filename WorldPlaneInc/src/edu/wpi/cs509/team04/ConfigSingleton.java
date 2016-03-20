93eb0a9eb8cafb86606ddfd6acca7f35acfb9ea1
Daniel <amoghimi@wpi.edu>
2016-03-20 14:16:40 -0500

    Add Comment to ConfigSingleton

--- WorldPlaneInc/src/edu/wpi/cs509/team04/ConfigSingleton.java
+++ WorldPlaneInc/src/edu/wpi/cs509/team04/ConfigSingleton.java
@@ -5,9 +5,21 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
+/**
+ * 
+ * Global Configuration Singleton * 
+ *
+ */
+
 public class ConfigSingleton {
 	private static ConfigSingleton instance = null;
 	private Properties props = new Properties();
+	
+	/**
+	 * Private default constructor which load the "config.properties" file into the memory
+	 * and initialize props object. 
+	 * 
+	 */
 	private ConfigSingleton(){
 		InputStream iStream;
 		try {
@@ -20,10 +32,22 @@ public class ConfigSingleton {
 		}		
 	}
 	
+	
+	/**
+	 * Get a String config value by providing a key 
+	 * 
+	 * @param key
+	 * @return
+	 */
 	public String get(String key){
 		return props.getProperty(key);
 	}
 	
+	/**
+	 * Return an instance of the singleton
+	 * 
+	 * @return
+	 */
 	public static ConfigSingleton getInstance(){
 		if(instance == null){
 			instance = new ConfigSingleton();
