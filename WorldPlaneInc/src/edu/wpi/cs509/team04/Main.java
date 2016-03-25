ae4be459382bc3dbdca8448b6c2ad2f067d2d8e0
Daniel <amoghimi@wpi.edu>
2016-03-24 19:06:25 -0500

    Add LocalTime DB Init to main

--- WorldPlaneInc/src/edu/wpi/cs509/team04/Main.java
+++ WorldPlaneInc/src/edu/wpi/cs509/team04/Main.java
@@ -15,6 +15,12 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		new Thread(new Runnable() {
+			public void run() {
+				LocalTime.init();
+			}
+		}).start();
+				
 		SearchModel model = new SearchModel();
 		ServerInterface resSys = new ServerInterface(model);
 		String team = ConfigSingleton.getInstance().get("team");
