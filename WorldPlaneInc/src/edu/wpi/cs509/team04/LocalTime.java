f51d1a6ed8def4a7d14aed9b78f2b4b6982e9ce7
Daniel <amoghimi@wpi.edu>
2016-04-14 16:09:39 -0500

    Convert Time function ready to go.

--- WorldPlaneInc/src/edu/wpi/cs509/team04/LocalTime.java
+++ WorldPlaneInc/src/edu/wpi/cs509/team04/LocalTime.java
@@ -2,6 +2,15 @@ package edu.wpi.cs509.team04;
 
 import java.util.*;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
 import com.google.gson.JsonElement;
 import com.google.gson.JsonIOException;
 import com.google.gson.JsonObject;
@@ -12,9 +21,11 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.StringReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -106,94 +117,164 @@ public class LocalTime{
 		return null;
 	}
 	
-	/**
-	 * This class has 3 parameters:longtitude, latitude and a given time 
-	 * It will return a time and a date of the depart airport compared with arrival airport
-	 * @param longtitute 
-	 * @param latitute
-	 * @param time
-	 * @return
-	 */
-	public static Date convert(double longtitute, double latitute, Date time){
-		String key = ConfigSingleton.getInstance().get("google-api-key");
-		String base = ConfigSingleton.getInstance().get("google-api-base");
-		String currentLoc = ConfigSingleton.getInstance().get("current-location");
-		
-		// Connection with API and get the content in the url provided longtitute and latitute
-		String surl = base + "?location="+latitute+","+longtitute+"&timestamp="+time.getTime()/1000+"&key=" + key;
-		final String mysurl = base + "?location=" + currentLoc + "&timestamp="+time.getTime()/1000+"&key=" + key;
-		
+	private static Document buildDomDoc (String xmlString) {
 		/**
-		 * Instantiate an rdate and url and check whether they are validated 
+		 * load the xml string into a DOM document and return the Document
 		 */
-		Date rdate = null;
-		URL url = null, myurl = null;
 		try {
-			url = new URL(surl);
-			myurl = new URL(mysurl);
-		} catch (MalformedURLException e) {			
+			DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
+			InputSource inputSource = new InputSource();
+			inputSource.setCharacterStream(new StringReader(xmlString));
+			
+			return docBuilder.parse(inputSource);
+		}
+		catch (ParserConfigurationException e) {
 			e.printStackTrace();
 			return null;
 		}
-		/**
-		 * Instantiate an request and myrequest and check whether they are validated 
-		 * If they are validated, build the connection to corresponding url
-		 */
-
-		HttpURLConnection request = null,  myrequest = null;
-		try {
-			request = (HttpURLConnection)url.openConnection();
-			myrequest = (HttpURLConnection)myurl.openConnection();
-		} catch (IOException e1) {
-			e1.printStackTrace();
+		catch (IOException e) {
+			e.printStackTrace();
 			return null;
 		}
-		
-	    try {
-			request.connect();
-			myrequest.connect();
-		} catch (IOException e) {
+		catch (SAXException e) {
 			e.printStackTrace();
 			return null;
-		}		    
-
-		// Convert to a JSON object to print data
-	    JsonParser jp = new JsonParser();
-	    JsonElement dat = null, mydat = null;
-		try {
-			//Convert the input stream to a json element
-			dat = jp.parse(new InputStreamReader((InputStream) request.getContent()));
-			mydat = jp.parse(new InputStreamReader((InputStream) myrequest.getContent())); 
-		} catch (JsonIOException e) {
-			e.printStackTrace();
-			return null;
-		} catch (JsonSyntaxException e) {
-			e.printStackTrace();
-			return null;
-		} catch (IOException e) {
-			e.printStackTrace();
-			return null;
-		} //Convert the input stream to a json element
-	    
-	    JsonObject dateobj = dat.getAsJsonObject(); //May be an array, may be an object. 
-	    JsonObject mydateobj = mydat.getAsJsonObject(); //May be an array, may be an object.
-	    long localTime;
-	    try{
-	    	/**
-			 * A formula to convert time between airports
-			 * The local time of a given location have fields:dstOffset,rawOffset,
-			 * status,timeZoneId and timeZoneName
-			 */
-	    	
-	    	localTime = time.getTime()-(mydateobj.get("rawOffset").getAsLong()-dateobj.get("rawOffset").getAsLong()+mydateobj.get("dstOffset").getAsLong()-dateobj.get("dstOffset").getAsLong())*1000;	
-	    }
-	    catch(Exception ex){
-	    	ex.printStackTrace();
-			return null;
-	    }
-	    rdate = new Date(localTime);
-		return rdate;
+		}
 	}
+	
+	private static Document getTimeDoc(double f, double g){
+		Path p = Paths.get("tmp\\" + f + ","+ g + ".xml");
+		try {
+			byte[] buffer =  Files.readAllBytes(p);
+			String s = new String(buffer, "UTF-8");
+			Document d = buildDomDoc(s);
+			NodeList raw_offset = d.getElementsByTagName("raw_offset");
+			NodeList dst_offset = d.getElementsByTagName("dst_offset");
+			if(raw_offset.getLength() > 0 && dst_offset.getLength() > 0){
+				return d;				
+			}
+			return null;
+						
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	public static Date Convert(Airport airport, Date time){
+    	//Date localTime = time.getTime()-(mydateobj.get("rawOffset").getAsLong()-dateobj.get("rawOffset").getAsLong()+mydateobj.get("dstOffset").getAsLong()-dateobj.get("dstOffset").getAsLong())*1000;
+		
+		Document dateObj = getTimeDoc(airport.latitude(), airport.longitude());
+		Double myLat = Double.parseDouble(ConfigSingleton.getInstance().get("current-lat"));
+		Double mylong = Double.parseDouble(ConfigSingleton.getInstance().get("current-long"));
+		Document myDateObj = getTimeDoc(myLat, mylong);
+		
+		long localTime = (long) (time.getTime()-
+		(			
+			Double.parseDouble(myDateObj.getElementsByTagName("raw_offset").item(0).getTextContent()) - 
+			Double.parseDouble(dateObj.getElementsByTagName("raw_offset").item(0).getTextContent()) + 
+			Double.parseDouble(myDateObj.getElementsByTagName("dst_offset").item(0).getTextContent()) - 
+			Double.parseDouble(dateObj.getElementsByTagName("dst_offset").item(0).getTextContent())
+		)*1000);
+//		
+//				
+//		NodeList raw_offset = myDateObj.getElementsByTagName("raw_offset");
+//		NodeList dst_offset = myDateObj.getElementsByTagName("dst_offset");
+//		System.out.println(raw_offset.item(0).getTextContent());	
+//		System.out.println(dst_offset.item(0).getTextContent());
+		return new Date(localTime);
+	}
+//	
+//	/**
+//	 * This class has 3 parameters:longtitude, latitude and a given time 
+//	 * It will return a time and a date of the depart airport compared with arrival airport
+//	 * @param longtitute 
+//	 * @param latitute
+//	 * @param time
+//	 * @return
+//	 */
+//	public static Date convert(double longtitute, double latitute, Date time){
+//		String key = ConfigSingleton.getInstance().get("google-api-key");
+//		String base = ConfigSingleton.getInstance().get("google-api-base");
+//		String currentLoc = ConfigSingleton.getInstance().get("current-location");
+//		
+//		// Connection with API and get the content in the url provided longtitute and latitute
+//		String surl = base + "?location="+latitute+","+longtitute+"&timestamp="+time.getTime()/1000+"&key=" + key;
+//		final String mysurl = base + "?location=" + currentLoc + "&timestamp="+time.getTime()/1000+"&key=" + key;
+//		
+//		/**
+//		 * Instantiate an rdate and url and check whether they are validated 
+//		 */
+//		Date rdate = null;
+//		URL url = null, myurl = null;
+//		try {
+//			url = new URL(surl);
+//			myurl = new URL(mysurl);
+//		} catch (MalformedURLException e) {			
+//			e.printStackTrace();
+//			return null;
+//		}
+//		/**
+//		 * Instantiate an request and myrequest and check whether they are validated 
+//		 * If they are validated, build the connection to corresponding url
+//		 */
+//
+//		HttpURLConnection request = null,  myrequest = null;
+//		try {
+//			request = (HttpURLConnection)url.openConnection();
+//			myrequest = (HttpURLConnection)myurl.openConnection();
+//		} catch (IOException e1) {
+//			e1.printStackTrace();
+//			return null;
+//		}
+//		
+//	    try {
+//			request.connect();
+//			myrequest.connect();
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//			return null;
+//		}		    
+//
+//		// Convert to a JSON object to print data
+//	    JsonParser jp = new JsonParser();
+//	    JsonElement dat = null, mydat = null;
+//		try {
+//			//Convert the input stream to a json element
+//			dat = jp.parse(new InputStreamReader((InputStream) request.getContent()));
+//			mydat = jp.parse(new InputStreamReader((InputStream) myrequest.getContent())); 
+//		} catch (JsonIOException e) {
+//			e.printStackTrace();
+//			return null;
+//		} catch (JsonSyntaxException e) {
+//			e.printStackTrace();
+//			return null;
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//			return null;
+//		} //Convert the input stream to a json element
+//	    
+//	    JsonObject dateobj = dat.getAsJsonObject(); //May be an array, may be an object. 
+//	    JsonObject mydateobj = mydat.getAsJsonObject(); //May be an array, may be an object.
+//	    long localTime;
+//	    try{
+//	    	/**
+//			 * A formula to convert time between airports
+//			 * The local time of a given location have fields:dstOffset,rawOffset,
+//			 * status,timeZoneId and timeZoneName
+//			 */
+//	    	
+//	    	localTime = time.getTime()-(mydateobj.get("rawOffset").getAsLong()-dateobj.get("rawOffset").getAsLong()+mydateobj.get("dstOffset").getAsLong()-dateobj.get("dstOffset").getAsLong())*1000;	
+//	    }
+//	    catch(Exception ex){
+//	    	ex.printStackTrace();
+//			return null;
+//	    }
+//	    rdate = new Date(localTime);
+//		return rdate;
+//	}
 	
 }
 
