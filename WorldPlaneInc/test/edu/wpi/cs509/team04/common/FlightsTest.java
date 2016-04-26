e348532232863966face3ce36c993cb4e1fd9a7d
Alexander W. Witt <awittscience@gmail.com>
2016-04-25 22:52:46 -0400

    - Updated FlightsTest to conform to ServerInterface calls

--- WorldPlaneInc/test/edu/wpi/cs509/team04/common/FlightsTest.java
+++ WorldPlaneInc/test/edu/wpi/cs509/team04/common/FlightsTest.java
@@ -5,7 +5,6 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import edu.wpi.cs509.team04.common.Flights;
-import edu.wpi.cs509.team04.resources.ConfigSingleton;
 import edu.wpi.cs509.team04.server.ServerInterface;
 
 public class FlightsTest {
@@ -13,8 +12,7 @@ public class FlightsTest {
 	@Test
 	public void testaddAll() {
 		ServerInterface resSys = ServerInterface.getInstance();
-		String team = ConfigSingleton.getInstance().get("team");
-		String xmlFlights = resSys.getFlights(team, "BOS", "2016_05_10");
+		String xmlFlights = resSys.getFlights("BOS", "2016_05_10");
 		
 		Flights flights = new Flights();
 		boolean AddSuccess=flights.addAll(xmlFlights);
